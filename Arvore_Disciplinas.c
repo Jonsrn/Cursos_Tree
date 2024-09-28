92dfb01894fee53d30c773de992ecdbb5297e094
Jonsrn <jonathansrn2@gmail.com>
2024-09-28 05:32:54 -0300

    Arquivo referente as funções de disciplinas
    A construção dos nos e das sub-arvores de disciplinas.

---
+++ Arvore_Disciplinas.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "universidade.h"
+
+
+// criar uma função de criar No de Disciplina
+
+//Criar uma função na Árvore de Disciplinas
+
+
+
+int criarNo_Disc(Inf_Disc *temp, Arv_disc **novo){ 
+    *novo = (Arv_disc*)malloc(sizeof(Arv_disc)); //Vai alocar a memória para o novo nó
+    int operacao = 1; 
+
+    if(*novo != NULL){
+        (*novo)->info.carga_horaria = temp->carga_horaria; 
+        snprintf((*novo)->info.nome_da_disciplina, sizeof((*novo)->info.nome_da_disciplina), "%s", temp->nome_da_disciplina);
+        (*novo)->info.codigo_disciplina = temp->codigo_disciplina; 
+        (*novo)->info.periodo = temp->periodo;  
+        (*novo)->esq = NULL; 
+        (*novo)->dir = NULL; 
+    }else{
+        operacao = 0; 
+    }  
+
+    return operacao; 
+
+}
+
+
+
+int inserirArvBB_Disc(Arv_disc **disciplinas, Arv_disc *novo) {
+    int operacao = 1; // Assume que a operação será bem-sucedida
+
+    // Se a subárvore estiver vazia, insira o novo nó
+    if (*disciplinas == NULL) {
+        *disciplinas = novo; // Insere a disciplina
+    } else {
+        // Compare o código da nova disciplina com o nó atual
+        if (novo->info.codigo_disciplina < (*disciplinas)->info.codigo_disciplina) {
+            // Inserir na subárvore esquerda
+            operacao = inserirArvBB_Disc(&(*disciplinas)->esq, novo);
+        } else if (novo->info.codigo_disciplina > (*disciplinas)->info.codigo_disciplina) {
+            // Inserir na subárvore direita
+            operacao = inserirArvBB_Disc(&(*disciplinas)->dir, novo);
+        } else {
+            // O código da disciplina já existe na árvore
+            operacao = 0; 
+        }
+    }
+
+    return operacao; // Retorna 0 ou 1 conforme a operação
+}
+
+
+
+
+ 
+
