45c8d3ec0307a9d664044655b45d4af5563dc2b2
Jonsrn <jonathansrn2@gmail.com>
2024-09-28 02:37:23 -0300

    Atualizando a main
    Adicionando a chamada da função de cadastro de alunos, e criação do Nó de alunos

--- main.c
+++ main.c
@@ -4,8 +4,10 @@
 
 int main(){
     int op1, op2;
-    Arv_cursos *raiz;  
-    raiz = NULL; 
+    Arv_cursos *cursos;  
+    cursos = NULL; 
+    No_Aluno *lista_alunos; 
+    lista_alunos = NULL; 
    
     do{
         menu_principal(); // Corrigido
@@ -17,15 +19,15 @@ int main(){
                 scanf("%d", &op2); 
                 switch(op2){
                    case 1: 
-                      //cadastrar aluno
+                      preencher_alunos(&lista_alunos, &cursos); 
                       break; 
                    case 2: 
                       //cadastrar curso
-                      preencher_cursos(&raiz); 
+                      preencher_cursos(&cursos); 
                       break;
                    case 3: 
                      //isso deve sair daqui, é temporario
-                      imprimirArvBB_Cursos(raiz); 
+                      imprimirArvBB_Cursos(cursos); 
                       break;     
                    default: 
                       break; 
@@ -64,49 +66,3 @@ int main(){
     return 0;
 }
 
-
-
-
-/*
-
-Menu Principal 
-
-1- Menu Cadastrar 
-2- Menu Exbir 
-3- Menu Remover
-4- Menu Testes 
-0- Sair
-
-Menu Cadastrar 
-
-1 - Cadastrar Alunos 
-2 - Cadastrar Cursos 
-3 - Cadastrar Disciplinas 
-4 - Cadastrar Matricula
-5 - Cadastrar Notas
-0 - Sair
-
-Menu Exibir 
-
-1 - Exibir Todos Alunos de Um Curso
-2 - Exibir Todos os cursus do Campus
-3 - Exibir Todas as Disciplinas de um Curso
-4 - Exibir Todas as Disciplinas de um Curso em um Periodo Especifico 
-5 - Exibir Todas as Disciplinas que um determinado Aluno está Matriculado
-6 - Exibir todas as notas de disciplinas de um determinado período de um determinado aluno.
-7 - Exibir nota de uma disciplina de um determinado aluno, mostrando o período e a carga horária da 
-disciplina.
-0 - Sair 
-
-Menu Remover 
-
-1 - Remover uma Disciplina de um Determinado Curso
-2 - Remover uma disciplina da árvore de matrícula de um determinado aluno.
-3 - Mostrar o histórico de um determinado aluno, contendo o nome do curso, as disciplinas e sua respectiva 
-nota organizadas pelo período que a disciplina está cadastrada no curso. 
-0 - Sair 
-
-
-
-
-*/
